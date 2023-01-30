ad97520dc98c95ebfe6ce8eae4c4f988cc0db595
dona6382 <46666529+dona6382@users.noreply.github.com>
2023-01-30 09:02:29 +0900

    card sales commit

--- src/ components/Card.js
+++ src/ components/Card.js
@@ -1,3 +1,9 @@
-function Card(){
-    return<div className={''}></div>
+import './Card.css';
+
+function Card(props){
+    const classes = 'card ' + props.className;
+
+    return <div className={classes}>{props.children}</div>
 }
+
+export default Card;
