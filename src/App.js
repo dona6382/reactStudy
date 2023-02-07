1bedbe43db0cfacda047ded8aaeb7724ec994986
dona6382 <46666529+dona6382@users.noreply.github.com>
2023-02-08 08:34:42 +0900

    reactstudy 1장 커밋

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import Expenses from "./ components/Expenses";
+import Expenses from "./components/Expenses/Expenses";
+import React from "react";
 
-function App() {
+const App = () => {
     const expenses = [
         {
             id: 'e1',
@@ -23,12 +24,20 @@ function App() {
         },
     ];
 
-  return (
-    <div>
-        <h2>Let's get started!!</h2>
-        <Expenses items={expenses}/>
-    </div>
-  );
+
+    return React.createElement(
+        'div',
+        {},
+        React.createElement('h2', {}, "Let's get started!!"),
+        React.createElement(Expenses, {items: expenses})
+    )
+
+  // return (
+  //   <div>
+  //       <h2>Let's get started!!</h2>
+  //       <Expenses items={expenses}/>
+  //   </div>
+  // );
 }
 
 export default App;
